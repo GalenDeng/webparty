e0f2fa0a7913344e3994eda9c01369a3f76db3bd
GalenDeng <827472476@qq.com>
2019-11-29 15:03:28 +0800

    修改 admin

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ django + djangorestframework + html  web聚会信息收集网站项目
 * [ajax请求的data数据格式:json、json字符串、标准参数](https://blog.csdn.net/qq_38180363/article/details/82177712)
 
 * [离线postman mac版download链接](https://blog.csdn.net/daicaho/article/details/81141379)
+
+* [使用的UI框架](https://element.eleme.cn/#/zh-CN/component/installation)
--- code/partygaosan5backend/signuprecord/admin.py
+++ code/partygaosan5backend/signuprecord/admin.py
@@ -3,6 +3,20 @@ from .models import *
 
 # Register your models here.
 
+@admin.register(PartyName)
+class MsgAdmin(admin.ModelAdmin):
+    # 显示数据库里面order出来的数据
+    list_display = ('name','telephone')
+    # 每页记录数
+    list_per_page = 25
+    pass
+
+    class Meta:
+        verbose_name = '报名记录'      # 重定义 MsgAdmin 的名字
+        verbose_name_plural = verbose_name
+
+
+
 '''
 class NameAdmin(admin.ModelAdmin):
     # 列表显示字段 name
